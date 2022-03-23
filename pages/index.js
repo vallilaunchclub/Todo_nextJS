a7756084cf8f2f07009a8640e0f09f8d84bbd6d5
vallilaunchclub <valli@thelaunchclub.in>
2022-03-23 12:07:20 +0530

    changed link import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 
 export default function Home() {
   return (
